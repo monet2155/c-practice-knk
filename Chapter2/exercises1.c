9e301864b2b44b4ec1591e2e67027f3641549c12
Monett <monet9812@gmail.com>
2023-11-18 00:18:56 +0900

    chapter 2 exercise 1 - hello world

---
+++ Chapter2/exercises1.c
@@ -0,0 +1,11 @@
+// Create and run Kernighan and Ritchie's "hello, world" program:
+// Do you get a warning message from the compiler? If so, what's needed to make it go away?
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+
+    // need this
+    return 0;
+}
