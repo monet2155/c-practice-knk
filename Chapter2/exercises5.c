878dbf396b424d5fa39c584b09c7ed52d55bdc1f
Monett <monet9812@gmail.com>
2023-11-18 00:45:54 +0900

    chapter 2 exercise 5 - Find not legal identifiers

---
+++ Chapter2/exercises5.c
@@ -0,0 +1,8 @@
+// Which of the following are not legal C identifiers?
+
+// a. 100_bottles
+// b. _100_bottles
+// c. one_hundred_bottles
+// d. bottles_by_the_hundred
+
+// a. 100_bottles is not a legal C identifier because it begins with a digit.
