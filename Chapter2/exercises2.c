1274236acc784787e217ddf763b648dec32224a3
Monett <monet9812@gmail.com>
2023-11-18 00:32:57 +0900

    chapter 2 exercise 2 - Consider program

---
+++ Chapter2/exercises2.c
@@ -0,0 +1,22 @@
+// Consider the following program:
+// (a) Identify the directives and statements in this program.
+// (b) What output does the program produce?
+
+// start directives
+#include <stdio.h>
+// end directives
+
+int main(void)
+{
+    // start statements
+    printf("Parkinson's Law:\nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+    return 0;
+    // end statements
+}
+
+// (b) The output of the program is:
+// Parkinson's Law:
+// Work expands so as to fill the time
+// available for its completion.
