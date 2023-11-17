19daaec4bcd54f563da8a29713bcd1ea66ac9283
Monett <monet9812@gmail.com>
2023-11-18 00:49:11 +0900

    chapter 2 exercise 6 - Why not good use more than one underscore

---
+++ Chapter2/exercises6.c
@@ -0,0 +1,4 @@
+// Why is it not a good idea for an identifier to contain more than one adjacent underscore (as in current__balance, for example)?
+
+// My Answer: Because it is useless...?
+// Copilot's Answer: It is not a good idea for an identifier to contain more than one adjacent underscore because it is easy to confuse with a single underscore.
