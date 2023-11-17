a22e6cac1df778759dd3b06a88b67a42376cefbd
Monett <monet9812@gmail.com>
2023-11-18 01:10:20 +0900

    chapter 2 exercise 7 - Which of the following are keywords in C?

---
+++ Chapter2/exercises7.c
@@ -0,0 +1,12 @@
+// Which of the following are keywords in C?
+
+// (a) for
+// (b) If
+// (c) main
+// (d) printf
+// (e) while
+
+// Answer: (a), (c), (e)
+
+// (b) : Not If, but if
+// (d) : printf is not C keyword, but it is a standard library function
