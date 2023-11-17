f5b03d964915e9b9289e58112761dbb64d53e858
Monett <monet9812@gmail.com>
2023-11-17 17:13:47 +0900

    chapter 2.3 - Comments

--- Chapter2/pun.c
+++ Chapter2/pun.c
@@ -1,7 +1,10 @@
+// Name: pun.c
+// Purpose: Prints a bad pun
+// Author: Monet
 #include <stdio.h>
 
 int main(void)
 {
-    printf("To C, or not to C: that is the question.\n");
+    printf("To C or not to C: that is the question.\n");
     return 0;
 }
