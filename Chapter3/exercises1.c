86be6b3b1ea19eedb820c4f5d83fb0a3b6e694d2
Monett <monet9812@gmail.com>
2023-11-19 19:29:54 +0900

    chapter 3 exercise 1 - printf produce

---
+++ Chapter3/exercises1.c
@@ -0,0 +1,13 @@
+// What output do the following calls of printf produce?
+
+// (a) printf("%6d,%4d", 86, 1040);
+// Answer : '    86,1040'
+
+// (b) printf("%12.5e", 30.253);
+// Answer : ' 3.02530e+01'
+
+// (c) printf("%.4f", 83.162);
+// Answer : '83.1620'
+
+// (d) printf("%-6.2g", .0000009979);
+// Answer : '1e-06 '
