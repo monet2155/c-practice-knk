99b862a85fa9f80099f8d5e5043ec974c7b0f373
Monett <monet9812@gmail.com>
2023-11-20 21:25:21 +0900

    chapter 4 exercise 3 - Negative divide on C89

---
+++ Chapter4/exercises3.c
@@ -0,0 +1,9 @@
+// What is the value of each of the following expressions in C89?
+// (Give all possible values if an expression may have more than one value)
+
+// (a) 8 / 5 : 1
+// (b) -8 / 5 : -1 or -2
+// (c) 8 / -5 : -1 or -2
+// (d) -8 / -5 : 1 or 2
+
+// Answer: b, c, d
