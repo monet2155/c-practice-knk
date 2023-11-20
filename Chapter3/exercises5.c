36c515e925264a470f546ff29fe1fa879f2dfa8b
Monett <monet9812@gmail.com>
2023-11-20 14:49:17 +0900

    chapter 3 exercise 5 - Predict scanf cal;l result 2

---
+++ Chapter3/exercises5.c
@@ -0,0 +1,11 @@
+// Suppose that we call scanf as follows:
+// scanf("%f%d%f", &x, &i, &y);
+// If the user enters
+// 12.3 45.6 789
+// what will be the values of x, i, and y after the call?
+// (Assume that x and y are float variables and i is an int variable.)
+
+// Answer
+// x : 12.3
+// i : 45
+// y : 0.6
