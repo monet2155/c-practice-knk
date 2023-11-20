0a76d8b4462374e306506a7627d40e0bb11df36a
Monett <monet9812@gmail.com>
2023-11-20 22:32:23 +0900

    chapter 4 exercise 15 - Predict i and j after executed expression

---
+++ Chapter4/exercises15.c
@@ -0,0 +1,11 @@
+// Give the values of i and j after each of the following expression statements has been executed.
+// (Assume that i has the value 1 initially and j has the value 2.)
+
+// (a) i += j;
+// Answer: '3 2'
+// (b) i--;
+// Answer: '0 2'
+// (c) i * j / i;
+// Answer: '1 2'
+// (d) i % ++j;
+// Answer: '1 3'
