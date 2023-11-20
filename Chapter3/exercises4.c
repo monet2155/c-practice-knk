44412e63e0316cd6e78b5fdd60152408b5fcc298
Monett <monet9812@gmail.com>
2023-11-20 14:46:28 +0900

    chapter 3 exercise 4 - Predict scanf call result

---
+++ Chapter3/exercises4.c
@@ -0,0 +1,11 @@
+// Suppose that we call scanf as follows:
+// scanf("%d%f%d", &i, &x, &j);
+// If the user enters
+// 10.3 5 6
+// what will be the values of i, x, and j after the call?
+// (Assume that i and j are int variables and x is a float variable.)
+
+// Answer
+// i : 10
+// x : 0.3
+// j : 5
