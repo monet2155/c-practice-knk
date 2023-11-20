5787db812a39633566f79705cba6b7b1dabfaa24
Monett <monet9812@gmail.com>
2023-11-21 00:53:27 +0900

    fix: not using split variable

--- Chapter4/programming_projects5.c
+++ Chapter4/programming_projects5.c
@@ -5,14 +5,18 @@
 
 int main(void)
 {
-    int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5,
-        first_sum, second_sum, total;
+    int upc;
+    int first_sum, second_sum, total;
 
     printf("Enter the first 11 digits of a UPC: ");
-    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &d, &i1, &i2, &i3, &i4, &i5, &j1, &j2, &j3, &j4, &j5);
+    scanf("%11d", &upc);
 
-    first_sum = d + i2 + i4 + j1 + j3 + j5;
-    second_sum = i1 + i3 + i5 + j2 + j4;
+    first_sum =
+        (upc / 10000000000) + ((upc / 100000000) % 10) + ((upc / 1000000) % 10) + ((upc / 10000) % 10) + ((upc / 100) % 10) + (upc % 10);
+
+    second_sum =
+        ((upc / 1000000000) % 10) + ((upc / 10000000) % 10) + ((upc / 100000) % 10) + ((upc / 1000) % 10) + ((upc / 10) % 10);
+
     total = 3 * first_sum + second_sum;
 
     printf("Check digit: %d\n", 9 - ((total - 1) % 10));
