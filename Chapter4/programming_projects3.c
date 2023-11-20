1321b67c002e5d57e0dcbf6a0b6f5fc8ab7471dc
Monett <monet9812@gmail.com>
2023-11-20 22:59:00 +0900

    chapter 4 project 3 - Reverse three digits number without arithmetic

---
+++ Chapter4/programming_projects3.c
@@ -0,0 +1,16 @@
+// Rewrite the program in Programming Project 2 so that it prints the reversal of a three-digit number without using arithmetic to split the number into digits.
+// Hint: See the upc.c program of Section 4.1
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n1, n2, n3;
+
+    printf("Enter a three-digit number: ");
+    scanf("%1d%1d%1d", &n1, &n2, &n3);
+
+    printf("The reversal is: %d%d%d", n3, n2, n1);
+
+    return 0;
+}
