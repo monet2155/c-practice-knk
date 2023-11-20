1ce55ba777755ce4555a2f515ef15146bffc462e
Monett <monet9812@gmail.com>
2023-11-20 14:52:22 +0900

    chapter 3 exercise 6 - modify addfrac.c

---
+++ Chapter3/exercises6.c
@@ -0,0 +1,28 @@
+// Show how to modify the addfrac.c program of Section 3.2 so that the user is allowed to enter fractions that contain spaces before and after each / character.
+
+#include <stdio.h>
+
+int main(void)
+{
+    int num1;
+    int denom1;
+    int num2;
+    int denom2;
+    int result_num;
+    int result_denom;
+
+    printf("Enter first fraction: ");
+    // modify this line
+    scanf("%d / %d", &num1, &denom1);
+
+    printf("Enter second fraction: ");
+    // modify this line
+    scanf("%d / %d", &num2, &denom2);
+
+    result_num = num1 * denom2 + num2 * denom1;
+    result_denom = denom1 * denom2;
+
+    printf("The sum is %d/%d\n", result_num, result_denom);
+
+    return 0;
+}
