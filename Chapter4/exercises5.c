90a5598f274176015e7e924d0842ccf19abea1da
Monett <monet9812@gmail.com>
2023-11-20 21:32:46 +0900

    chapter 4 exercise 5 - Negative remainder

---
+++ Chapter4/exercises5.c
@@ -0,0 +1,7 @@
+// What is the value of each of the following expressions in C89?
+// (Give all possible values if an expression may have more than one value)
+
+// (a) 8 % 5    : 3
+// (b) -8 % 5   : -3 or 5
+// (c) 8 % -5   : 3 or -5
+// (d) -8 % -5  : 3 or 5
