fe4c26e4cb3773fb8be90a18f3147467b08c1d0f
Monett <monet9812@gmail.com>
2023-11-22 00:33:05 +0900

    chapter 5 exercise 7 - conditional expressions

---
+++ Chapter5/exercises7.c
@@ -0,0 +1,7 @@
+// What does the following statement print if i has the value 17?
+// What does it print if i has the value -17?
+
+// printf("%d", i >= 0 ? i : -i);
+
+// i == 17 : 17
+// i == -17 : 17
