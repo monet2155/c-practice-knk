ac7d2a57407e899f184d5038427751ebe90eb1fe
Monett <monet9812@gmail.com>
2023-11-22 00:37:42 +0900

    chapter 5 exercise 8 - Simplify statement

---
+++ Chapter5/exercises8.c
@@ -0,0 +1,11 @@
+// The following if statement is unnecessarily complicated.
+// Simplify it as much as possible.
+// (Hint: The entire statement can be replaced by a single assignment.)
+
+int main(void)
+{
+    int age = 10;
+    int teenager;
+
+    teenager = age >= 13 && age <= 19;
+}
