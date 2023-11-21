4ae506c8772590a07f8cb6aa53b1d7108cb50355
Monett <monet9812@gmail.com>
2023-11-22 00:46:37 +0900

    chapter 5 exercise 10 - Switch statement result

---
+++ Chapter5/exercises10.c
@@ -0,0 +1,22 @@
+// What output does the following program fragment produce?
+// (Asuume that i is an integer variable.)
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 1;
+    switch (i % 3)
+    {
+    case 0:
+        printf("zero");
+    case 1:
+        printf("one");
+    case 2:
+        printf("two");
+    }
+
+    return 0;
+}
+
+// program print zeroonetwo
