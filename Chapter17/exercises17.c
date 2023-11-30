8f4c3dc7e99cc666c7e3ca14df69ec1358e73166
Monett <monet9812@gmail.com>
2023-11-30 20:01:11 +0900

    chapter 17 exercise 17 - How to use qsort

---
+++ Chapter17/exercises17.c
@@ -0,0 +1,7 @@
+/**
+ * Let a be an array of 100 integers.
+ * Write a call of qsort that sorts only the last 50 elements in a.
+ * (You don't need to write the comparison function).
+ */
+
+// qsort(&(a[50]), 50, sizeof(a[0]), compare);
