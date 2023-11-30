90aa6f32f231879e888b48e35d665d024fe5204f
monet2155 <monet9812@gmail.com>
2023-11-30 15:20:24 +0900

    chapter 17 exercise 5 - Find legal statements in struct

---
+++ Chapter17/exercises5.c
@@ -0,0 +1,23 @@
+/**
+ * Suppose that f and p are declared as follows:
+ */
+
+struct
+{
+    union
+    {
+        char a, b;
+        int c;
+    } d;
+    int e[5];
+} f, *p = &f;
+
+/**
+ * Which of the following statements are legal?
+ * (a) p->b = ' ';
+ * (b) p->e[3] = 10;
+ * (c) (*p).d.a = '*';
+ * (d) p->d->c = 20;
+ *
+ * Answer : (b) (c)
+ */
