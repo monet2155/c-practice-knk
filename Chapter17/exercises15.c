65a2c65aa62b176eb750ba9a02041885ac58da16
Monett <monet9812@gmail.com>
2023-11-30 19:48:59 +0900

    chapter 17 exercise 15 - Function pointer

---
+++ Chapter17/exercises15.c
@@ -0,0 +1,30 @@
+// Show the output of the following program and explain what it does.
+
+// -12, -10, -6, 0 (n is 3)
+
+#include <stdio.h>
+
+int f1(int (*f)(int));
+int f2(int i);
+
+int main(void)
+{
+    printf("Answer: %d\n", f1(f2));
+
+    return 0;
+}
+
+int f1(int (*f)(int))
+{
+    int n = 0;
+    while ((*f)(n))
+    {
+        n++;
+    }
+    return n;
+}
+
+int f2(int i)
+{
+    return i * i + i - 12;
+}
