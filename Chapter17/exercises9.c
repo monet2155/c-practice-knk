a3a080bc07b3ad22efbe96e89fbb61e3e01fdfd3
Monett <monet9812@gmail.com>
2023-11-30 18:22:58 +0900

    chapter 17 exercise 9 - True or false with structure pointer

---
+++ Chapter17/exercises9.c
@@ -0,0 +1,4 @@
+// True or false: If x is a structure and a is a member of that structure,
+// then (&x)->a is the same as x.a. Justify your answer.
+
+// Answer: Same. If x is structure variable, &x is equal to structure pointer. And x.a is also legal.
